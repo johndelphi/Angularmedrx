9e11925c233a17555ad79a68ad6105e719abea6f
Ja-ci <130248891+Ja-ci@users.noreply.github.com>
2023-06-15 14:24:22 +0300

    header

--- my-app/src/app/userdetails-signup/userdetails-signup.component.ts
+++ my-app/src/app/userdetails-signup/userdetails-signup.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class UserdetailsSignupComponent {
 
+  wordToHighlight = 'Free';
+  firstName!: string;
+  lastName!: string;
+  dateOfbirth!: string;
+  passWord!: string;
+
+  onSubmit() {
+    // Perform the sign-in logic here
+    console.log('First name:', this.firstName);
+    console.log('last name:', this.lastName);
+    console.log('Date of birth:', this.dateOfbirth);
+    console.log('Password:', this.passWord);
+    // Add your sign-in logic here, such as making an API request or validating credentials
+  }
 }
