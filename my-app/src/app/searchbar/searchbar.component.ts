cb6511bc9782fa63017f1df52dd2c7c10b8b918f
john michael <57584587+johndelphi@users.noreply.github.com>
2023-08-15 13:48:26 -0500

    Revert "loading functionality complete"

--- my-app/src/app/searchbar/searchbar.component.ts
+++ my-app/src/app/searchbar/searchbar.component.ts
@@ -7,7 +7,6 @@ import { debounceTime, switchMap } from 'rxjs/operators';
 import { AppState } from '../State/app.state';
 import { Store } from '@ngrx/store';
 import { MedicineSearchresults } from '../State/Medicinesearchresults.Actions';
-
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
@@ -33,17 +32,19 @@ export class SearchbarComponent {
 
   onSearch() {
     this.isLoading = true;
-
+    setTimeout(() => {
     console.log('Search term:', this.searchTerm);
     this.backendService.search(this.searchTerm).subscribe(response => {
       console.log('Response:', response);
       if (Array.isArray(response)) {
         this.store.dispatch(MedicineSearchresults({ results: response }));
-        this.router.navigate(['/results']);
-        console.log(this.store.select(state => state.search.searchResults));
-      }
       
-      this.isLoading = false; // Stop loading screen after results are displayed
+     // this.store.dispatch( MedicineSearchresults({results: response}));
+      this.router.navigate(['/results']);
+      console.log(this.store.select(state => state.search.searchResults));
+      }
     });
+  this.isLoading=false;
+  }, 2000);
   }
 }
