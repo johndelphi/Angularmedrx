6a20572c81ad6f37c295ae075053ae6e50d5f284
Ja-ci <130248891+Ja-ci@users.noreply.github.com>
2024-06-19 20:41:30 +0300

    landing page

---
+++ my-app/src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css'
+})
+export class LandingPageComponent {
+  landingpage = [
+    {
+      title: 'Empowering Pharmacies through Mobile Solutions In Africa',
+      image: 'assets/Images/pic4.jpg',
+      description: 'Download Our App', },
+  ]
+
+  constructor() {}
+  ngOnInit(): void {}
+}
