3dc533702786c8509a8cd26070a0a55fda6a6d51
Ja-ci <130248891+Ja-ci@users.noreply.github.com>
2024-09-05 14:27:07 +0300

    header/half landing page

--- my-app/src/app/landing-page/landing-page.component.ts
+++ my-app/src/app/landing-page/landing-page.component.ts
@@ -6,22 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
-  landingpage = [
-    {
-      title: 'Empowering Pharmacies through Mobile Solutions in Africa',
-      image: 'assets/Images/pic4.jpg',
-      description: 'Download Our App',
-    },
-  ];
 
-  download = [
-    {
-      google: 'assets/Images/googleplay1.png',
-      apple: 'assets/Images/appstore21.png',
-    },
-  ];
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
