5b9a7e8d690701cd608403e88b111da253d72037
johnmichael <johnmickey281@gmail.com>
2024-10-11 21:07:31 +0300

    feat: Add download app component and styles

---
+++ my-app/src/app/downloadapp/downloadapp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-downloadapp',
+  templateUrl: './downloadapp.component.html',
+  styleUrls: ['./downloadapp.component.css']
+})
+export class DownloadappComponent {
+
+}
