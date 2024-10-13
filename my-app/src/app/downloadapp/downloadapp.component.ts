a12c9fd7fa47e72d949c908b77b77030445d6890
johnmichael <johnmickey281@gmail.com>
2024-10-13 04:46:07 +0300

    checkick point still working on the refinment

--- my-app/src/app/downloadapp/downloadapp.component.ts
+++ my-app/src/app/downloadapp/downloadapp.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./downloadapp.component.css']
 })
 export class DownloadappComponent {
+downloadApp() {
+throw new Error('Method not implemented.');
+}
 
 }
